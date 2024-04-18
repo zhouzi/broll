9a2f6f3f0fd62eff9c8286aa14aa80709036de94
Gabin Aureche <hello@gabinaureche.com>
2024-04-18 13:56:05 +0800

    Workaround build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    // TODO: a build error is raised when minification is enabled: Nn is not defined
+    //       couldn't track down what is breaking and why yet
+    config.optimization.minimize = false;
+    return config;
+  },
+};
 
 export default nextConfig;
