c81e15d739bbc90824d3cd417b120ec62376b3f6
Gabin Aureche <hello@gabinaureche.com>
2024-04-19 12:35:17 +0800

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { Outfit as FontSans } from "next/font/google";
 
 import { Toaster } from "@/components/ui/sonner";
@@ -33,6 +34,7 @@ export default function RootLayout({
       >
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
