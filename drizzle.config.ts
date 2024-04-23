e853a985f22e5932d717b2631f7cd51eb36890e9
Gabin Aureche <hello@gabinaureche.com>
2024-04-23 23:14:49 +0800

    Add migration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,9 @@ import { Config } from "drizzle-kit";
 import { env } from "./lib/env";
 
 export default {
-  schema: "./lib/db/schema.ts",
   driver: "pg",
+  schema: "./lib/db/schema.ts",
+  out: "./lib/db/migrations",
   dbCredentials: {
     host: env.POSTGRES_HOST,
     port: env.POSTGRES_PORT,
---
+++ lib/db/migrations/0000_organic_paper_doll.sql
@@ -0,0 +1,53 @@
+CREATE TABLE IF NOT EXISTS "account" (
+	"userId" text NOT NULL,
+	"type" text NOT NULL,
+	"provider" text NOT NULL,
+	"providerAccountId" text NOT NULL,
+	"refresh_token" text,
+	"access_token" text,
+	"expires_at" integer,
+	"token_type" text,
+	"scope" text,
+	"id_token" text,
+	"session_state" text,
+	CONSTRAINT "account_provider_providerAccountId_pk" PRIMARY KEY("provider","providerAccountId")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "session" (
+	"id" text PRIMARY KEY NOT NULL,
+	"sessionToken" text NOT NULL,
+	"userId" text NOT NULL,
+	"expires" timestamp NOT NULL,
+	CONSTRAINT "session_sessionToken_unique" UNIQUE("sessionToken")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "user" (
+	"id" text PRIMARY KEY NOT NULL,
+	"name" text,
+	"email" text NOT NULL,
+	"emailVerified" timestamp,
+	"image" text,
+	CONSTRAINT "user_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "verificationToken" (
+	"identifier" text NOT NULL,
+	"token" text NOT NULL,
+	"expires" timestamp NOT NULL,
+	CONSTRAINT "verificationToken_identifier_token_pk" PRIMARY KEY("identifier","token"),
+	CONSTRAINT "verificationToken_token_unique" UNIQUE("token")
+);
+--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "account_userId_index" ON "account" ("userId");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "session_userId_index" ON "session" ("userId");--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "account" ADD CONSTRAINT "account_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "session" ADD CONSTRAINT "session_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
